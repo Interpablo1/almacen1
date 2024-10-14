082ca0133c3300ecb1922d21a5b31663f4b03e07
ReynaV19 <reynavasva15outlook@gmail.com>
2024-10-14 13:49:43 -0600

    entidades iniciales

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+import { AlmacenesModule } from './almacenes/almacenes.module';
+import { ProveedoresModule } from './proveedores/proveedores.module';
+import { ProductosModule } from './productos/productos.module';
+import { InventarioModule } from './inventario/inventario.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AlmacenesModule,
+    ProveedoresModule,
+    ProductosModule,
+    InventarioModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
